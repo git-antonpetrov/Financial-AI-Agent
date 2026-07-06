ebc8129dc5c235a24ace4adee7347db8655594e6
Anton Petrov <anton@dennisovich.ru>
2026-07-06 18:31:28 +0300

    Венес логику получения данные в отдельный контейнер

--- config/chroma_config.py
+++
@@ -1,7 +0,0 @@
-import os
-# pyrefly: ignore [missing-import]
-import chromadb
-
-def get_chroma_client():
-    """Инициализирует и возвращает клиент векторной базы данных."""
-    return chromadb.HttpClient(host="localhost", port=8000)
